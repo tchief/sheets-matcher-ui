14e8cf5495a1d70df0afaf814895dce132fa7232
tchief <cyber.tchief@gmail.com>
2022-03-24 12:19:29 +0200

    Airtable: multiple sources same type support added.

--- src/match/airtable.ts
+++ src/match/airtable.ts
@@ -27,23 +27,31 @@ const getRows = async (
   VIEW_ID: string,
   ids: number[] = []
 ) => {
-  const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(BASE_ID);
-  const rows: any[] = [];
-  await base(VIEW_ID)
-    // TODO: Pass fields from matchRequest to retieve, not all of them
-    // TODO: Pass view as param, or default view: 'Matcher', sort: [{ field: 'ID', direction: 'desc' }]
-    // TODO: Max records as param
-    .select({ view: 'Matcher', maxRecords: 1000 })
-    .eachPage(function page(fetchedRecords, fetchNextPage) {
-      rows.push(...fetchedRecords);
-      fetchNextPage();
-    });
+  const baseIds = BASE_ID.split(',');
+  const viewIds = VIEW_ID.split(',');
 
-  // TODO: Unify id / rowIndex props.
-  const filteredRows = ids?.length
-    ? rows.filter((row) => ids.includes(row.rowIndex ?? row.id))
-    : rows;
-  return filteredRows?.map(mapRow);
+  const allRows = await Promise.all(baseIds.map(async (baseId, index) => {
+    const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(baseId);
+    const rows: any[] = [];
+
+    await base(viewIds[index])
+      // TODO: Pass fields from matchRequest to retieve, not all of them
+      // TODO: Pass view as param, or default view: 'Matcher', sort: [{ field: 'ID', direction: 'desc' }]
+      // TODO: Max records as param
+      .select({ view: 'Matcher', maxRecords: 1000 })
+      .eachPage(function page(fetchedRecords, fetchNextPage) {
+        rows.push(...fetchedRecords);
+        fetchNextPage();
+      });
+
+    // TODO: Unify id / rowIndex props.
+    const filteredRows = ids?.length
+      ? rows.filter((row) => ids.includes(row.rowIndex ?? row.id))
+      : rows;
+    return filteredRows?.map(mapRow);
+  }));
+
+  return allRows.flat();
 };
 
 export const getAirtableRetrieveFuncs = {
