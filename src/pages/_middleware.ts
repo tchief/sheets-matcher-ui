d40e6f1d6c55e970f6916b6e8557c7ababca1e61
tchief <cyber.tchief@gmail.com>
2022-03-26 19:15:41 +0200

    Middleware: redirect bots.

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
-import { BLOCKED_COUNTRIES, REDIRECT_TO } from '../utils/blocked'
-import type { NextRequest } from 'next/server'
+import { BLOCKED_COUNTRIES, BLOCKED_IPS, REDIRECT_TO } from '../utils/blocked'
+import type { NextRequest } from 'next/server';
+import isbot from 'isbot';
 
 export function middleware(req: NextRequest) {
-    const country = req.geo?.country || 'US'
+    const country = req.geo?.country || 'US';
+    const isBot = !req.ua?.ua.includes('https://vercel.com') && (!!req.ua?.isBot || isbot(req.ua?.ua));
+    const ip = req.ip || '127.0.0.1';
 
-    if (BLOCKED_COUNTRIES.includes(country)) {
+    if (BLOCKED_COUNTRIES.includes(country) || isBot || BLOCKED_IPS.includes(ip)) {
         return NextResponse.redirect(REDIRECT_TO)
     }
 
--- src/utils/blocked.ts
+++ src/utils/blocked.ts
@@ -1,2 +1,3 @@
 export const BLOCKED_COUNTRIES = process.env.BLOCKED_COUNTRIES?.split(',') ?? [];
+export const BLOCKED_IPS = process.env.BLOCKED_IPS?.split(',') ?? [];;
 export const REDIRECT_TO = process.env.REDIRECT_BLOCKED_COUNTRY_TO ?? '/404';
