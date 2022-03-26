8d0a00dbf01cc0b8c18839d0444b09d58f6e2fa0
tchief <cyber.tchief@gmail.com>
2022-03-26 14:52:38 +0200

    Set cookies to 7 days.

--- src/pages/api/auth/[...supabase].ts
+++ src/pages/api/auth/[...supabase].ts
@@ -1,3 +1,6 @@
 import { handleAuth } from '@supabase/supabase-auth-helpers/nextjs';
 
-export default handleAuth({ logout: { returnTo: '/' } });
+export default handleAuth({
+    logout: { returnTo: '/' },
+    cookieOptions: { lifetime: 7 * 24 * 60 * 60 }
+});
